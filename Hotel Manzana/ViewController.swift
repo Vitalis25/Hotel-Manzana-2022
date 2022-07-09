631c85f4d7c628ce0364ec3ca61270f788df0a62
Vitaly <lub-olen@yandex.ru>
2022-07-09 16:06:14 +0300

    Initial Commit

---
+++ Hotel Manzana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel Manzana
+//
+//  Created by Vitally Ochnev on 09.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
